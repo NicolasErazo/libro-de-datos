ab74faa2e91342bc8090b1c65d5a2d96266c5db8
Nicolas Erazo <nico.lacho@outlook.com>
2022-11-09 15:20:22 -0500

    Full Update

--- libro/views.py
+++ libro/views.py
@@ -1,49 +1,108 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from fpdf import FPDF 
+from django.http import HttpResponse
+from datetime import datetime
 
 from libro.forms import crearAfiliado
 from libro.models import Afiliado
 
-# Create your views here.
+# Generate PDF
+class PDF(FPDF):
+    def header(self):
+        # Logo
+        self.image('libro\static\img\Escudo.png', 10, 8, 33)
+        # Arial bold 15
+        self.set_font('Arial', 'B', 15)
+        # Move to the right
+        self.cell(80)
+        # Title
+        self.cell(10, 10, 'CONSTANCIA DE AFILIACIÓN', 0, 0)
+        # Line break
+        self.ln(20)
+
+    # Page footer
+    def footer(self):
+        # Position at 1.5 cm from bottom
+        self.set_y(-15)
+        # Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Page number
+        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+
+@login_required
+def afiliado_pdf(request, afiliado_id):
+
+    afiliados = get_object_or_404(Afiliado, pk=afiliado_id, user=request.user)
+    NombreMayus = afiliados.nombres.upper()
+    now = datetime.now()
+
+    pdf = PDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font('Arial', '', 12)
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,f'La Junta De Accion Comunal BARRIO PABLO SEXTO del municipio de Acacias - Meta, a solicitud del interesado informa que el(la) Señor(a) {NombreMayus}, identificado(a) con la cédula de ciudadania número {afiliados.numero_documento} está afiliado(a) a esta Organización de Acción Comunal desde el {afiliados.created}, en el folio 2 con el número de registro 2 del libro de afiliados.',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,f'Se expide en el municipio de Acacias el día {now.day} de {now.month} de {now.year}. Puede verificar la autenticidad de este documento ingresando a la pagina web http://orso.meta.gov.co en la sección de trámites en línea y digitar el siguiente código: 0DBet7JfHWLBdcq',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.multi_cell(0,10,'',0, 'J')
+    pdf.set_font('Arial', 'B', 12)
+    pdf.multi_cell(0,10,'______________________________         ______________________________',0, 'C')
+    pdf.multi_cell(0,10,'ANSELMO QUEVEDO                                 CLAUDIA PATRICIA PAEZ',0, 'C')
+    pdf.multi_cell(0,10,'PRESIDENTE(A)                                           SECRETARIO(A)',0, 'C')
+
+    response = HttpResponse(pdf.output(dest='S').encode('latin-1'), content_type='application/pdf')
+    response['Content-Disposition'] = f"attachment; filename=Afiliado_{afiliados.nombres}.pdf"
+    return response
+
 def home(request):
     return render(request, 'home.html')
+
 
 @login_required
 def afiliados(request):
     search = request.GET.get("buscar")
-    afiliados = Afiliado.objects.filter(user=request.user, datecompleted__isnull=True)
+    afiliados = Afiliado.objects.filter(
+        user=request.user, datecompleted__isnull=True)
     if search:
         afiliados = Afiliado.objects.filter(
-            Q(nombres__icontains = search) |
-            Q(tipo_documento__icontains = search) |
-            Q(numero_documento__icontains = search) |
-            Q(sexo__icontains = search) |
-            Q(telefono__icontains = search) |
-            Q(email__icontains = search) |
-            Q(direccion__icontains = search) |
-            Q(educacion__icontains = search) |
-            Q(ocupacion__iexact = search) |
-            Q(discapacidad__iexact = search) |
-            Q(grupo_etnico__iexact = search) |
-            Q(comision_trabajo__iexact = search) |
-            Q(revision_secretarial__iexact = search) |
-            Q(dignatario__iexact = search) |
-            Q(cargo__iexact = search) |
-            Q(observaciones__icontains = search) 
+            Q(nombres__icontains=search) |
+            Q(tipo_documento__icontains=search) |
+            Q(numero_documento__icontains=search) |
+            Q(sexo__icontains=search) |
+            Q(telefono__icontains=search) |
+            Q(email__icontains=search) |
+            Q(direccion__icontains=search) |
+            Q(educacion__icontains=search) |
+            Q(ocupacion__iexact=search) |
+            Q(discapacidad__iexact=search) |
+            Q(grupo_etnico__iexact=search) |
+            Q(comision_trabajo__iexact=search) |
+            Q(revision_secretarial__iexact=search) |
+            Q(dignatario__iexact=search) |
+            Q(cargo__iexact=search) |
+            Q(observaciones__icontains=search)
 
         )
     return render(request, 'afiliados.html', {'afiliados': afiliados})
 
+
 @login_required
 def afiliado_create(request):
     if request.method == 'GET':
-        return render(request, 'afiliado_create.html',{
+        return render(request, 'afiliado_create.html', {
             'form': crearAfiliado
         })
     else:
@@ -53,34 +112,38 @@ def afiliado_create(request):
             nuevo_afiliado.user = request.user
             nuevo_afiliado.save()
             return redirect('afiliados')
-            
+
         except ValueError:
-            return render(request, 'afiliado_create.html',{
+            return render(request, 'afiliado_create.html', {
                 'form': crearAfiliado,
                 'error': 'Por favor proporcione datos válidos'
             })
 
         except IntegrityError:
-            return render(request, 'afiliado_create.html',{
+            return render(request, 'afiliado_create.html', {
                 'form': crearAfiliado,
                 'error': 'Por favor seleccione una fecha de nacimiento real'
-            })   
+            })
+
 
 @login_required
 def afiliado_detail(request, afiliado_id):
     if request.method == 'GET':
-        afiliado = get_object_or_404(Afiliado, pk=afiliado_id, user=request.user)
+        afiliado = get_object_or_404(
+            Afiliado, pk=afiliado_id, user=request.user)
         form = crearAfiliado(instance=afiliado)
-        return render(request, 'afiliado_detail.html',{'afiliado':afiliado, 'form': form})
+        return render(request, 'afiliado_detail.html', {'afiliado': afiliado, 'form': form})
     else:
         try:
-            afiliado = get_object_or_404(Afiliado, pk=afiliado_id, user=request.user)
+            afiliado = get_object_or_404(
+                Afiliado, pk=afiliado_id, user=request.user)
             form = crearAfiliado(request.POST, instance=afiliado)
             form.save()
             return redirect('afiliados')
         except ValueError:
-            return  render(request, 'afiliado_detail.html',{'afiliado':afiliado, 'form': form,
-            'error': 'Error actualizando Afiliado'}) 
+            return render(request, 'afiliado_detail.html', {'afiliado': afiliado, 'form': form,
+                                                            'error': 'Error actualizando Afiliado'})
+
 
 @login_required
 def afiliado_completed(request, afiliado_id):
@@ -90,25 +153,22 @@ def afiliado_completed(request, afiliado_id):
         afiliado.save()
         return redirect('afiliados')
 
+
 @login_required
 def afiliados_completed(request):
-    afiliados = Afiliado.objects.filter(user=request.user, datecompleted__isnull=False).order_by
+    afiliados = Afiliado.objects.filter(
+        user=request.user, datecompleted__isnull=False).order_by
     ('-datecompletedx')
     return render(request, 'afiliados.html', {'afiliados': afiliados})
+
 
 @login_required
 def afiliado_delete(request, afiliado_id):
     afiliado = get_object_or_404(Afiliado, pk=afiliado_id, user=request.user)
     if request.method == 'POST':
         afiliado.delete()
-        return redirect('afiliados') 
+        return redirect('afiliados')
 
-@login_required
-def afiliado_pdf(request, afiliado_id):
-    afiliado = get_object_or_404(Afiliado, pk=afiliado_id, user=request.user)
-    if request.method == 'POST':
-        #Codigo PDF
-        return redirect('afiliados')           
 
 def signup(request):
 
@@ -138,21 +198,22 @@ def signup(request):
 
 def signin(request):
     if request.method == 'GET':
-        return render(request, 'signin.html',{
+        return render(request, 'signin.html', {
             'form': AuthenticationForm
         })
     else:
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'signin.html',{
+            return render(request, 'signin.html', {
                 'form': AuthenticationForm,
                 'error': 'Username or Password is incorrect'
             })
         else:
             login(request, user)
             return redirect('home')
-       
+
+
 @login_required
 def exit(request):
     logout(request)
