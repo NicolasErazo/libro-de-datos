f176831c55546c7537a0ff943663ee2b4b513859
Nicolas Erazo <nico.lacho@outlook.com>
2022-11-23 14:24:05 -0500

    Update

--- libro/admin.py
+++ libro/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Afiliado
+from .models import Usuario
 
 class AfiliadosAdmin(admin.ModelAdmin):
     readonly_fields = ('created',)
 
+
 # Register your models here.
 admin.site.register(Afiliado, AfiliadosAdmin) 
+admin.site.register(Usuario) 
